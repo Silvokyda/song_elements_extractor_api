28ad4ae16cf3e73b5d244fdace5a5af96685492c
Silvokyda <deseve48@gmail.com>
2024-04-22 10:57:30 +0300

    hypercorn to uvicorn

--- main.py
+++ main.py
@@ -66,5 +66,5 @@ def separate():
     return jsonify(result)
 
 if __name__ == '__main__':
-    import hypercorn
-    hypercorn.run(app, host="0.0.0.0", port=5000)
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
